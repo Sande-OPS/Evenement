1decf9a4eb46b89e7be29d427797376c8252b1f4
Sande <philomensande@gmail.com>
2023-01-11 20:18:31 +0000

    version sans php et logo

--- navigation.php
+++
@@ -1,8 +0,0 @@
-<nav>
-		<ul>
-			<li> <a href="index.html">Accueil</a>  </li>
-			<li> <a href="Connection.php">Compte</a> </li>
-			<li> <a href="#">Contact</a></li>
-			<li> <a href="lieux.php" class="EcranLarge">About-Us</a> </li>
-		</ul>
-</nav>
